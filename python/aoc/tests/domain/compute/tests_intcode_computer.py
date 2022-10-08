5219144f19a80b0bc67928f50ec85bfc3e034d38
Sam Denison <sdenison@lmi.org>
2022-10-08 12:49:03 -0500

    SD - aoc#9 - Getting day 2 step 2 solution.

--- python/aoc/tests/domain/compute/tests_intcode_computer.py
+++ python/aoc/tests/domain/compute/tests_intcode_computer.py
@@ -45,6 +45,29 @@ class TestsIntcodeUnit(unittest.TestCase):
         computedoutput = computer.RunProgram(program, None)
         self.assertEqual(4138687, computedoutput[0])
 
+    def test_can_get_final_answer_for_day2_step_2(self):
+        candidateProgram = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 6, 1, 19, 1, 19, 10, 23, 2, 13, 23, 27, 1, 5, 27, 31, 2,
+            6, 31, 35, 1, 6, 35, 39, 2, 39, 9, 43, 1, 5, 43, 47, 1, 13, 47, 51, 1, 10, 51, 55, 2, 55, 10, 59, 2, 10, 59,
+            63, 1, 9, 63, 67, 2, 67, 13, 71, 1, 71, 6, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 9, 87, 1, 6, 87, 91, 2,
+            91, 6, 95, 1, 95, 6, 99, 2, 99, 13, 103, 1, 6, 103, 107, 1, 2, 107, 111, 1, 111, 9, 0, 99, 2, 14, 0, 0]
+        computer = IntcodeComputer()
+        solutioncount = 0
+        for noun in range(100):
+            for verb in range(100):
+                candidateProgram[1] = noun
+                candidateProgram[2] = verb
+                computedOutput = computer.RunProgram(candidateProgram, None)
+                if (computedOutput[0] == 19690720):
+                    solutioncount = solutioncount + 1
+                    solutionNoun = noun
+                    solutionVerb = verb
+        #Curious if there was more than one solution
+        self.assertEqual(1, solutioncount)
+        self.assertEqual(66, solutionNoun)
+        self.assertEqual(35, solutionVerb)
+        solutionValue = (100 * solutionNoun) + solutionVerb
+        self.assertEqual(6635, solutionValue)
+
     def test_something(self):
         self.assertEqual(True, True)
 
