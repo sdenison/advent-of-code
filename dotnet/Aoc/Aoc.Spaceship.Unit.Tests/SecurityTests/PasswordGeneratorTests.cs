b0be759c24e3bf41ec1c05e36ad697ad1c43e0f9
Sam Denison <sdenison@lmi.org>
2022-10-21 16:18:24 -0500

    SD - aoc#16 - Day 4 puzzle.

---
+++ dotnet/Aoc/Aoc.Spaceship.Unit.Tests/SecurityTests/PasswordGeneratorTests.cs
@@ -0,0 +1,52 @@
+﻿using Aoc.Spaceship.Security;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace Aoc.Spaceship.Unit.Tests.SecurityTests
+{
+    [TestFixture]
+    public class PasswordGeneratorTests
+    {
+        [Test]
+        public void Can_create_password_generator()
+        {
+            var passwordGenerator = new PasswordGenerator();
+        }
+
+        [Test]
+        [TestCase(654321, false)]
+        [TestCase(135679, false)] //no duplicates
+        [TestCase(111111, false)]
+        [TestCase(111123, false)]
+        [TestCase(112345, true)]
+        [TestCase(223450, false)]
+        [TestCase(123789, false)]
+        public void Can_determine_if_numbers_match_rules(int candidateNumber, bool shouldPassValidation)
+        {
+            var passwordGenerator = new PasswordGenerator();
+            var passedValidation = passwordGenerator.Check(candidateNumber);
+            Assert.AreEqual(shouldPassValidation, passedValidation);
+        }
+
+        [Test]
+        public void Can_get_candidate_passwords()
+        {
+            var passwordGenerator = new PasswordGenerator();
+            var candidates = passwordGenerator.GetCandidatePasswords(245182, 790572);
+            Assert.AreEqual(710, candidates.Count);
+        }
+
+        [Test]
+        [TestCase(112233, true)]
+        [TestCase(123444, false)]
+        [TestCase(111122, true)]
+        [TestCase(113339, true)]
+        public void Numbers_only_repeat_once(int candidateNumber, bool shouldPassValidation)
+        {
+            var passwordGenerator = new PasswordGenerator();
+            var passedValidation = passwordGenerator.Check(candidateNumber);
+            Assert.AreEqual(shouldPassValidation, passedValidation);
+        }
+            
+    }
+}
---
+++ dotnet/Aoc/Aoc.Spaceship/Security/PasswordGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Aoc.Spaceship.Security
+{
+    public class PasswordGenerator
+    {
+        public bool Check(int candidate)
+        {
+            var passesTests = NumberHasDuplicateDigit(candidate);
+            passesTests = passesTests && DigitsAlwaysIncrease(candidate);
+            return passesTests;
+        }
+
+        public bool NumberHasDuplicateDigit(int candidate)
+        {
+            var candidateString = candidate.ToString();
+            var matchingDigits = new List<char>();
+            var hasDouble = false;
+            foreach (var candidateChar in candidateString.ToCharArray())
+            {
+                if (matchingDigits.Count > 0)
+                {
+                    if (matchingDigits[0] == candidateChar)
+                    {
+                        matchingDigits.Add(candidateChar);
+                    }
+                    else
+                    {
+                        if (matchingDigits.Count == 2)
+                            hasDouble = true;
+                        matchingDigits.Clear();
+                        matchingDigits.Add(candidateChar);
+                    }
+                }
+                else
+                {
+                    matchingDigits.Add(candidateChar);
+                }
+            }
+
+            if (matchingDigits.Count == 2)
+                hasDouble = true;
+            return hasDouble;
+        }
+
+        public bool DigitsAlwaysIncrease(int candidate)
+        {
+            var candidateString = candidate.ToString();
+            var biggestNumber = 0;
+            foreach (var candidateChar in candidateString.ToCharArray())
+            {
+                var digit = int.Parse(candidateChar.ToString());
+                if (biggestNumber > digit)
+                    return false;
+                biggestNumber = digit;
+            }
+            return true;
+        }
+
+        public List<int> GetCandidatePasswords(int startIndex, int endIndex)
+        {
+            var candidates = new List<int>();
+            for (var candidate = startIndex; candidate <= endIndex; candidate++)
+            {
+                if (Check(candidate))
+                    candidates.Add(candidate);
+            }
+            return candidates;
+        }
+    }
+}
