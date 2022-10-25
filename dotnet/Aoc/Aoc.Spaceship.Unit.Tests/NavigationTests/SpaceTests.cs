2677e573587a9721ac0eb3b787e2a86f0af4ff76
Sam Denison <sdenison@lmi.org>
2022-10-24 21:09:31 -0500

    SD - aoc#18 - Getting the maximum number of orbits needed.

--- dotnet/Aoc/Aoc.Spaceship.Unit.Tests/NavigationTests/SpaceTests.cs
+++ dotnet/Aoc/Aoc.Spaceship.Unit.Tests/NavigationTests/SpaceTests.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection.Metadata.Ecma335;
 using Aoc.Spaceship.Navigation;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NUnit.Framework;
 
 namespace Aoc.Spaceship.Unit.Tests.NavigationTests
@@ -14,12 +11,93 @@ namespace Aoc.Spaceship.Unit.Tests.NavigationTests
         [Test]
         public void Can_create_space()
         {
-            char[,] simpleMap = new char[,]
+            char[,] emptyMap = new char[,]
             {
                 {'.', '.'},
                 {'.', '.'},
             };
-            var space = new Space(simpleMap);
+            var space = new Space(emptyMap);
+            Assert.IsNotNull(space);
         }
+
+        [Test]
+        public void Can_get_max_orbitsl()
+        {
+            char[,] emptyMap = 
+            {
+                {'#', '#'},
+                {'#', '#'},
+            };
+            var space = new Space(emptyMap);
+            var coordinateToTest = new Coordinate(0, 0);
+            var expectedMaxOrbits = 1;
+            Assert.AreEqual(expectedMaxOrbits, space.MaxOrbits(coordinateToTest));
+        }
+
+        [Test]
+        public void Can_get_max_orbits2()
+        {
+            char[,] emptyMap =
+            {
+                {'#', '#', '.', '.'},
+                {'#', '#', '.', '.'},
+                {'#', '#', '.', '.'},
+                {'#', '#', '.', '.'},
+            };
+            var space = new Space(emptyMap);
+            var coordinateToTest = new Coordinate(0, 0);
+            var expectedMaxOrbits = 3;
+            Assert.AreEqual(expectedMaxOrbits, space.MaxOrbits(coordinateToTest));
+
+            //change coordinates
+            coordinateToTest = new Coordinate(2, 1);
+            expectedMaxOrbits = 2;
+            Assert.AreEqual(expectedMaxOrbits, space.MaxOrbits(coordinateToTest));
+
+            //change coordinates
+            coordinateToTest = new Coordinate(2, 3);
+            expectedMaxOrbits = 3;
+            Assert.AreEqual(expectedMaxOrbits, space.MaxOrbits(coordinateToTest));
+
+            //change coordinates
+            coordinateToTest = new Coordinate(3, 2);
+            expectedMaxOrbits = 3;
+            Assert.AreEqual(expectedMaxOrbits, space.MaxOrbits(coordinateToTest));
+        }
+
+        [Test]
+        public void Can_get_visible_asteroids_3_by_3()
+        {
+            var eMap = new[] {new[] {
+                '#', '.', '#'
+            }};
+            char[,] emptyMap = new char[,]
+            {
+                {'#', '.', '#'},
+                {'.', '.', '#'},
+                {'#', '#', '#'},
+            };
+            var space = new Space(emptyMap);
+            var coordinateToTest = new Coordinate(0, 0);
+            var visibleAsteroids = space.GetVisibleAsteroids(coordinateToTest);
+
+            //Starting from immediately to the right
+            coordinateToTest = new Coordinate(1, 0);
+            visibleAsteroids = space.GetVisibleAsteroids(coordinateToTest);
+        }
+
+        //[Test]
+        //public void Can_get_visible_asteroids_3_by_3()
+        //{
+        //    char[,] emptyMap = new char[,]
+        //    {
+        //        {'#', '.', '#'},
+        //        {'.', '.', '#'},
+        //        {'#', '#', '#'},
+        //    };
+        //    var space = new Space(emptyMap);
+        //    var coordinateToTest = new Coordinate(0, 0);
+        //    var visibleAsteroids = space.GetVisibleAsteroids(coordinateToTest);
+        //}
     }
 }
--- dotnet/Aoc/Aoc.Spaceship/Navigation/Space.cs
+++ dotnet/Aoc/Aoc.Spaceship/Navigation/Space.cs
@@ -1,12 +1,40 @@
-﻿namespace Aoc.Spaceship.Navigation
+﻿using System.Collections.Generic;
+
+namespace Aoc.Spaceship.Navigation
 {
     public class Space
     {
         private char[,] _map;
+        private int MapWidth => _map.GetLength(0);
 
         public Space(char[,] map)
         {
             _map = map;
+        }
+
+        public IList<Coordinate> GetVisibleAsteroids(Coordinate coordinate)
+        {
+            var maxOrbits = MaxOrbits(coordinate);
+            for(var orbit = 0; orbit < maxOrbits; orbit++)
+            {
+            }
+            var coordinates = new List<Coordinate>();
+            return coordinates;
+        }
+
+        public int MaxOrbits(Coordinate coordinate)
+        {
+            var xSize= _map.GetLength(0);
+            var ySize = _map.GetLength(1);
+            var maxOrbits = coordinate.X;
+            //if (maxOrbits < xSize - 1)
+            if (maxOrbits < (xSize - coordinate.X) - 1)
+                maxOrbits = (xSize - coordinate.X) - 1;
+            if (maxOrbits < coordinate.Y)
+                maxOrbits = coordinate.Y;
+            if (maxOrbits < (ySize - coordinate.Y) - 1)
+                maxOrbits = (ySize - coordinate.Y) - 1;
+            return maxOrbits;
         }
     }
 }
