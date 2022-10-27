12777a64e54443ebafa962a9bad42e77639c79c6
Sam Denison <sdenison@lmi.org>
2022-10-27 18:40:37 -0500

    SD - aoc#19 - Getting first part of day 8.

--- dotnet/Aoc/Aoc.Spaceship.Unit.Tests/ImagingTests/ImageTests.cs
+++ dotnet/Aoc/Aoc.Spaceship.Unit.Tests/ImagingTests/ImageTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Aoc.Spaceship.Imaging;
+using NUnit.Framework;
 
 namespace Aoc.Spaceship.Unit.Tests.ImagingTests
 {
@@ -26,6 +27,41 @@ namespace Aoc.Spaceship.Unit.Tests.ImagingTests
             var image = new Imaging.Image(imageData, height, width);
             Assert.AreEqual(new[] {1, 2, 3}, image.Layers[0].Rows[0]);
             Assert.AreEqual(new[] {0, 1, 2}, image.Layers[1].Rows[1]);
+        }
+
+        [Test]
+        public void Can_solve_day_8_step_1()
+        {
+            var imageData = DataGenerator.GetDay8PuzzleData();
+            var height = 6;
+            var width = 25;
+            var image = new Imaging.Image(imageData, height, width);
+            Layer layerWithFewest0Dights = null;
+            var fewestZeros = int.MaxValue;
+            var numberOfOneDigits = 0;
+            var numberOfTwoDigits = 0;
+            foreach(var layer in image.Layers)
+            {
+
+                var oneDigitsForLayer = 0;
+                var twoDigitsForLayer = 0;
+                var zeroCount = 0;
+                foreach(var row in layer.Rows)
+                {
+                    zeroCount += row.Where(x => x.Equals(0)).Count();
+                    oneDigitsForLayer += row.Where(x => x.Equals(1)).Count();
+                    twoDigitsForLayer += row.Where(x => x.Equals(2)).Count();
+                }
+                if (fewestZeros > zeroCount)
+                {
+                    fewestZeros = zeroCount;
+                    numberOfOneDigits = oneDigitsForLayer;
+                    numberOfTwoDigits = twoDigitsForLayer;
+                }
+            }
+
+            var answer = numberOfOneDigits * numberOfTwoDigits;
+            Assert.AreEqual(1072, answer);
         }
     }
 }
