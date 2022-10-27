a3d65e7be674a4be095b25cfa0358daa70b16d0d
Sam Denison <sdenison@lmi.org>
2022-10-27 17:39:30 -0500

    SD - aoc#19 - First stab at an Image and the test for it.

---
+++ dotnet/Aoc/Aoc.Spaceship.Unit.Tests/ImagingTests/ImageTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Aoc.Spaceship.Unit.Tests.ImagingTests
+{
+    [TestFixture]
+    public class ImageTests
+    {
+        [Test]
+        public void Can_parse_image_data()
+        {
+            var imageData = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2};
+            var height = 2;
+            var width = 3;
+            var image = new Imaging.Image(imageData, height, width);
+            Assert.AreEqual(new[] {1, 2, 3}, image.Layers[0].Rows[0]);
+            Assert.AreEqual(new[] {0, 1, 2}, image.Layers[1].Rows[1]);
+        }
+    }
+}
---
+++ dotnet/Aoc/Aoc.Spaceship/Imaging/Image.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Aoc.Spaceship.Utilities;
+
+namespace Aoc.Spaceship.Imaging
+{
+    public class Image
+    {
+        public IList<Layer> Layers { get; }
+
+        public Image(int[] imageData, int height, int width)
+        {
+            Layers = new List<Layer>();
+            var imagePointer = 0;
+            while (imagePointer < imageData.Length)
+            {
+                var layer = new Layer();
+                for(int i=1; i <= height; i++)
+                {
+                    layer.AddRow(imageData.SubArray(imagePointer, width));
+                    imagePointer += width;
+                }
+                Layers.Add(layer);
+            }
+        }
+    }
+}
