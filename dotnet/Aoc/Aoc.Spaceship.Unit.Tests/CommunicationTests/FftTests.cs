1b481c171f5e32b6f8423440998df7b0a8f6c71f
Sam Denison <sdenison@lmi.org>
2022-11-03 17:46:44 -0500

    SD - aoc#21 - Trying to run things in parallel.

--- dotnet/Aoc/Aoc.Spaceship.Unit.Tests/CommunicationTests/FftTests.cs
+++ dotnet/Aoc/Aoc.Spaceship.Unit.Tests/CommunicationTests/FftTests.cs
@@ -31,55 +31,119 @@ namespace Aoc.Spaceship.Unit.Tests.CommunicationTests
         }
 
         [Test]
+        public void Can_get_offset_with_just_math()
+        {
+            var input = "15243";
+            int[] pattern = { 1, 2, 3 };
+            var fft = new Fft(input, pattern);
+            //Assert.AreEqual(2, fft.GetPatternInt(1, 0));
+            //Assert.AreEqual(3, fft.GetPatternInt(1, 1));
+            //Assert.AreEqual(1, fft.GetPatternInt(1, 2));
+            //Assert.AreEqual(2, fft.GetPatternInt(1, 3));
+            //Assert.AreEqual(3, fft.GetPatternInt(1, 4));
+            //Assert.AreEqual(1, fft.GetPatternInt(1, 5));
+
+            var expectedTwoPattern = new[] { 1, 1, 2, 2, 3, 3 };
+            Assert.AreEqual(1, fft.GetPatternInt(2, 0)); //0
+            Assert.AreEqual(2, fft.GetPatternInt(2, 1)); //0
+            Assert.AreEqual(2, fft.GetPatternInt(2, 2)); //1
+            Assert.AreEqual(3, fft.GetPatternInt(2, 3)); //1
+            Assert.AreEqual(3, fft.GetPatternInt(2, 4)); //2
+            Assert.AreEqual(1, fft.GetPatternInt(2, 5)); //2
+            Assert.AreEqual(1, fft.GetPatternInt(2, 6)); //1
+            Assert.AreEqual(2, fft.GetPatternInt(2, 7)); //1
+            Assert.AreEqual(2, fft.GetPatternInt(2, 8)); //1
+        }
+
+        [Test]
         public void Can_apply_phase_1()
         {
             var input = "12345678";
             int[] pattern = { 0, 1, 0, -1 };
             var fft = new Fft(input, pattern);
-            fft.ApplyPhase(1);
+            fft.ApplyPhases(1);
             int[] afterOnePhase = {4, 8, 2, 2, 6, 1, 5, 8};
             Assert.AreEqual(afterOnePhase, fft.PhaseData[1]);
         }
 
-        [Test]
-        public void Can_apply_phase_4()
-        {
-            var input = "12345678";
-            int[] pattern = { 0, 1, 0, -1 };
-            var fft = new Fft(input, pattern);
-            fft.ApplyPhases(4);
-            int[] afterOnePhase = {4, 8, 2, 2, 6, 1, 5, 8};
-            Assert.AreEqual(afterOnePhase, fft.PhaseData[1]);
-            int[] afterTwoPhases= { 3, 4, 0, 4, 0, 4, 3, 8 };
-            Assert.AreEqual(afterTwoPhases, fft.PhaseData[2]);
-            int[] afterThreePhases= { 0, 3, 4, 1, 5, 5, 1, 8};
-            Assert.AreEqual(afterThreePhases, fft.PhaseData[3]);
-            int[] afterFourPhases = {0, 1, 0, 2, 9, 4, 9, 8};
-            Assert.AreEqual(afterFourPhases, fft.PhaseData[4]);
-        }
+        //[Test]
+        //public void Can_apply_phase_4()
+        //{
+        //    var input = "12345678";
+        //    int[] pattern = { 0, 1, 0, -1 };
+        //    var fft = new Fft(input, pattern);
+        //    fft.ApplyPhases(4);
+        //    int[] afterOnePhase = {4, 8, 2, 2, 6, 1, 5, 8};
+        //    Assert.AreEqual(afterOnePhase, fft.PhaseData[1]);
+        //    int[] afterTwoPhases= { 3, 4, 0, 4, 0, 4, 3, 8 };
+        //    Assert.AreEqual(afterTwoPhases, fft.PhaseData[2]);
+        //    int[] afterThreePhases= { 0, 3, 4, 1, 5, 5, 1, 8};
+        //    Assert.AreEqual(afterThreePhases, fft.PhaseData[3]);
+        //    int[] afterFourPhases = {0, 1, 0, 2, 9, 4, 9, 8};
+        //    Assert.AreEqual(afterFourPhases, fft.PhaseData[4]);
+        //}
 
-        [Test]
-        public void Can_apply_100_phases_from_example()
+        //[Test]
+        //public void Can_apply_100_phases_from_example()
+        //{
+        //    var input = "80871224585914546619083218645595";
+        //    int[] pattern = { 0, 1, 0, -1 };
+        //    var fft = new Fft(input, pattern);
+        //    fft.ApplyPhases(100);
+        //    int[] expectedOutput = {2, 4, 1, 7, 6, 1, 7, 6};
+        //    Assert.AreEqual(expectedOutput, fft.PhaseData[100].ToArray().SubArray(0, 8));
+        //}
+
+        //[Test]
+        //public void Can_apply_100_phases_from_example2()
+        //{
+        //    var input = "19617804207202209144916044189917";
+        //    int[] pattern = { 0, 1, 0, -1 };
+        //    var fft = new Fft(input, pattern);
+        //    fft.ApplyPhases(100);
+        //    int[] expectedOutput = {7, 3, 7, 4, 5, 4, 1, 8};
+        //    var subArray = fft.PhaseData[2].ToArray<int>();
+        //    Assert.AreEqual(expectedOutput, fft.PhaseData[100].ToArray().SubArray(0, 8));
+        //}
+
+        ////[Test, Ignore("too long")]
+        [Test] //Takes 15 seconds
+        public void Can_get_day_16_step_1_answer()
         {
-            var input = "80871224585914546619083218645595";
+            var input = GetDay16PuzzleData();
             int[] pattern = { 0, 1, 0, -1 };
             var fft = new Fft(input, pattern);
             fft.ApplyPhases(100);
-            int[] expectedOutput = {2, 4, 1, 7, 6, 1, 7, 6};
-            var subArray = fft.PhaseData[2].ToArray<int>();
-            Assert.AreEqual(expectedOutput, fft.PhaseData[100].ToArray().SubArray(0, 8));
+            var puzzleAnswer = fft.PhaseData[100].ToArray().SubArray(0, 8);
+            int[] expectedOutput = { 4, 9, 2, 5, 4, 7, 7, 9 };
+            Assert.AreEqual(expectedOutput, puzzleAnswer);
         }
 
-        [Test]
-        public void Can_apply_100_phases_from_example2()
+        //[Test]
+        //public void Can_get_message_offset()
+        //{
+        //    var input = "03036732577212944063491565474664";
+        //    var finalInput = "";
+        //    for (var i = 0; i < 10000; i++)
+        //        finalInput += input;
+        //    int[] pattern = { 0, 1, 0, -1 };
+        //    var fft = new Fft(finalInput, pattern);
+        //    fft.ApplyPhases(1);
+        //    //int[] expectedOffset = {0, 3, 0, 3, 6, 7, 3};
+        //    int[] expectedOffset = { 0, 3, 0, 3, 6, 7, 3 };
+
+        //    var charOffset = 303673;
+        //    var listOffset = 303673 / finalInput.Length;
+        //    var finalOffset = 303673 % finalInput.Length;
+        //    var intList = fft.PhaseData[listOffset];
+        //    var message = intList.ToArray().SubArray(finalOffset, 8).ToString();
+        //    Assert.AreEqual("", message);
+        //}
+
+        private string GetDay16PuzzleData()
         {
-            var input = "19617804207202209144916044189917";
-            int[] pattern = { 0, 1, 0, -1 };
-            var fft = new Fft(input, pattern);
-            fft.ApplyPhases(100);
-            int[] expectedOutput = {7, 3, 7, 4, 5, 4, 1, 8};
-            var subArray = fft.PhaseData[2].ToArray<int>();
-            Assert.AreEqual(expectedOutput, fft.PhaseData[100].ToArray().SubArray(0, 8));
+            return
+                "59793513516782374825915243993822865203688298721919339628274587775705006728427921751430533510981343323758576985437451867752936052153192753660463974146842169169504066730474876587016668826124639010922391218906707376662919204980583671961374243713362170277231101686574078221791965458164785925384486127508173239563372833776841606271237694768938831709136453354321708319835083666223956618272981294631469954624760620412170069396383335680428214399523030064601263676270903213996956414287336234682903859823675958155009987384202594409175930384736760416642456784909043049471828143167853096088824339425988907292558707480725410676823614387254696304038713756368483311";
         }
     }
 }
--- dotnet/Aoc/Aoc.Spaceship/Communication/Fft.cs
+++ dotnet/Aoc/Aoc.Spaceship/Communication/Fft.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Aoc.Spaceship.Communication
 {
@@ -25,58 +26,117 @@ namespace Aoc.Spaceship.Communication
             Pattern = pattern;
         }
 
-        public int[] GetPattern(int phase)
+        public int[] GetPattern(int phaseStep)
         {
-            var newPattern = new List<int>();
-            foreach (var patternItem in Pattern)
+            //var newPattern = new List<int>();
+            var newPattern = new int[Pattern.Length * phaseStep];
+            var count = 0;
+            for (var i = 1; i <= Pattern.Length; i++)
             {
-                for(int j = 0; j < phase; j++)
+                for(int j = 1; j <= phaseStep; j++)
                 {
-                    newPattern.Add(patternItem);
+                    //newPattern.Add(Pattern[i]);
+                    newPattern[count] = Pattern[i - 1];
+                    count++;
                 }
             }
-            return newPattern.ToArray();
+            return newPattern;
         }
 
         public void ApplyPhases(int numberOfPhases)
         {
             for (var phase = 1; phase <= numberOfPhases; phase++)
-                ApplyPhase(phase);
+                ApplyPhase(phase).Wait();
         }
 
-        public void ApplyPhase(int phase)
+        public async Task ApplyPhase(int phase)
         {
             var phaseData = new List<int>();
-            for(var i = 1; i <= PhaseData[phase - 1].Count; i++)
+            var applyPhaseStepTasks = new List<Task<int>>();
+
+            //Task.Run
+
+            //Task.Run
+
+            var range = Enumerable.Range(1, PhaseData[phase - 1].Count);
+            var tasks = range.Select(i => ApplyPhaseStep(i, PhaseData[phase - 1]));
+            var results = await Task.WhenAll(tasks);
+            foreach (var result in results)
             {
-                if (i == 3)
-                {
-                    var x = "got here";
-                }
-                var phaseInt = ApplyPhaseStep(i, PhaseData[phase - 1]);
-                phaseData.Add(phaseInt);
+                phaseData.Add(result);
             }
+
+            //for(var i = 1; i <= PhaseData[phase - 1].Count; i++)
+            //{
+                    
+            //    //var phaseInt = ApplyPhaseStep(i, PhaseData[phase - 1]);
+            //    //applyPhaseStepTasks.Add(Task.Run((int) ApplyPhaseStep(i, PhaseData[phase - 1])));
+            //    applyPhaseStepTasks.Add(ApplyPhaseStep(i, PhaseData[phase - 1]));
+            //    //phaseData.Add(phaseInt);
+            //}
+
+            //Task.WaitAll(applyPhaseStepTasks.ToArray());
+            //foreach (var taskInt in applyPhaseStepTasks)
+            //{
+            //    phaseData.Add(taskInt.Result);
+            //}
+
             PhaseData.Add(phaseData);
         }
 
-        public int ApplyPhaseStep(int phaseStep, IList<int> initialPhaseData)
+        public async Task<int> ApplyPhaseStep(int phaseStep, IList<int> initialPhaseData)
         {
-            var phasePattern = GetPattern(phaseStep);
-            var output = new List<int>();
             var patternPointer = 0;
-            //var offset = phaseStep == 1 ? 0 : 1;
-            var offset = 1;
-            foreach(var phaseInput in initialPhaseData)
+            var sum = 0;
+            var phaseDataCount = 0;
+
+            for (var i = 0; i < initialPhaseData.Count; i++)
             {
-                output.Add(phaseInput * phasePattern[patternPointer + offset]);
+                var phaseInput = initialPhaseData[i];
+                sum += phaseInput * GetPatternInt(phaseStep, i);
                 patternPointer++;
-                if (patternPointer + offset == phasePattern.Length)
-                {
-                    patternPointer = 0;
-                    offset = 0;
-                }
             }
-            return Math.Abs(output.Sum() % 10);
+
+            //var range = Enumerable.Range(0, initialPhaseData.Count - 1);
+            //await Task.Run(() => Parallel.ForEach(range, i =>
+            //{
+            //    var phaseInput = initialPhaseData[i];
+            //    sum += phaseInput * GetPatternInt(phaseStep, i);
+            //    //patternPointer++;
+            //}));
+
+
+            //await Task.Run(() => Parallel.ForEach(initialPhaseData, phaseInput =>
+            //{
+            //    sum += phaseInput * GetPatternInt(phaseStep, patternPointer);
+            //    patternPointer++;
+            //}));
+
+            //for (var i = 0; i < initialPhaseData.Count; i++)
+            //    {
+            //        var phaseInput = initialPhaseData[i];
+            //        sum += phaseInput * GetPatternInt(phaseStep, patternPointer);
+            //        patternPointer++;
+            //    }
+
+
+
+            //await Task.Run(() =>
+            //{
+            //    for (var i = 0; i < initialPhaseData.Count; i++)
+            //    {
+            //        var phaseInput = initialPhaseData[i];
+            //        sum += phaseInput * GetPatternInt(phaseStep, patternPointer);
+            //        patternPointer++;
+            //    }
+            //});
+            return Math.Abs(sum % 10);
+        }
+
+        public int GetPatternInt(int phaseStep, int index)
+        {
+            var rIndex = ((index + 1) % (phaseStep * Pattern.Length)) / phaseStep;
+            return Pattern[rIndex];
         }
     }
 }
