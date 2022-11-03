fafe6db54ebf0ff2f9ff62e5e5254a20c730c655
Sam Denison <sdenison@lmi.org>
2022-11-02 21:08:02 -0500

    SD - aoc#12 - Can calculate first phase.

---
+++ dotnet/Aoc/Aoc.Spaceship.Unit.Tests/CommunicationTests/FftTests.cs
@@ -0,0 +1,61 @@
+﻿using Aoc.Spaceship.Communication;
+using NUnit.Framework;
+
+namespace Aoc.Spaceship.Unit.Tests.CommunicationTests
+{
+    [TestFixture]
+    public class FftTests
+    {
+        [Test]
+        public void Can_work_with_phases()
+        {
+            var input = "15243";
+            int[] pattern = { 1, 2, 3 };
+            var fft = new Fft(input, pattern);
+            //The first item in our phase data should be 1
+            Assert.AreEqual(1, fft.PhaseData[0][0]);
+        }
+
+        [Test]
+        public void Can_work_with_patterns()
+        {
+            var input = "15243";
+            int[] pattern = { 1, 2, 3 };
+            var fft = new Fft(input, pattern);
+            var expectedOnePattern = new[] { 1, 2, 3 };
+            Assert.AreEqual(expectedOnePattern, fft.GetPattern(1));
+            var expectedTwoPattern = new[] { 1, 1, 2, 2, 3, 3 };
+            Assert.AreEqual(expectedTwoPattern, fft.GetPattern(2));
+        }
+
+        [Test]
+        public void Can_apply_pattern_to_input()
+        {
+            var input = "15243";
+            int[] pattern = { -1, 2, 3 };
+            var fft = new Fft(input, pattern);
+            //1*1, 5*2, 2*3, 4*1, 3*2
+            int[] afterFirstPhase = { 1, 0, 6, 4, 6 };
+            fft.PhaseData.Add(afterFirstPhase);
+            var sumAfterPhaseStep1 = fft.ApplyPhaseStep(1, fft.PhaseData[0]);
+            Assert.AreEqual(7, sumAfterPhaseStep1);
+            //New Pattern should be 1, 1, 2, 2, 3, 3
+            //When the pattern is applied it should be 1, 2, 2, 3, 3
+            //-1*1, 5*2, 2*2, 4*3, 3*3
+            int[] afterSecondPhase = { 1, 0, 2, 2, 8 };
+            var sumAfterPhaseStep2 = fft.ApplyPhaseStep(2, fft.PhaseData[0]);
+            Assert.AreEqual(4, sumAfterPhaseStep2);
+        }
+
+        [Test]
+        public void Can_apply_phase_1()
+        {
+            var input = "12345678";
+            int[] pattern = { 0, 1, 0, -1 };
+            var fft = new Fft(input, pattern);
+            fft.ApplyPhase(1);
+            int[] afterOnePhase = {4, 8, 2, 2, 6, 1, 5, 8};
+            Assert.AreEqual(afterOnePhase, fft.PhaseData[1]);
+        }
+    }
+}
---
+++ dotnet/Aoc/Aoc.Spaceship/Communication/Fft.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aoc.Spaceship.Communication
+{
+    //Flawed Frequency Transmission algorithm
+    public class Fft
+    {
+        public IList<IList<int>> PhaseData { get; }
+        public int[] Pattern { get; }
+
+        public Fft(string initialTransmission, int[] pattern) : this(initialTransmission.ToCharArray(), pattern)
+        {
+        }
+
+        public Fft(char[] initialTransmission, int[] pattern) : this(initialTransmission.Select(x => int.Parse(x.ToString())).ToList(), pattern)
+        {
+        }
+
+        public Fft(IList<int> initialTransmission, int[] pattern)
+        {
+            PhaseData = new List<IList<int>> {initialTransmission};
+            Pattern = pattern;
+        }
+
+        public int[] GetPattern(int phase)
+        {
+            var newPattern = new List<int>();
+            foreach (var patternItem in Pattern)
+            {
+                for(int j = 0; j < phase; j++)
+                {
+                    newPattern.Add(patternItem);
+                }
+            }
+            return newPattern.ToArray();
+        }
+
+
+        public void ApplyPhase(int phase)
+        {
+            var phaseData = new List<int>();
+            for(var i = 1; i <= PhaseData[0].Count; i++)
+            {
+                if (i == 3)
+                {
+                    var x = "got here";
+                }
+                var phaseInt = ApplyPhaseStep(i, PhaseData[phase - 1]);
+                phaseData.Add(phaseInt);
+            }
+            PhaseData.Add(phaseData);
+        }
+
+        public int ApplyPhaseStep(int phaseStep, IList<int> initialPhaseData)
+        {
+            var phasePattern = GetPattern(phaseStep);
+            var output = new List<int>();
+            var patternPointer = 0;
+            var offset = phaseStep == 1 ? 0 : 1;
+            foreach(var phaseInput in initialPhaseData)
+            {
+                output.Add(phaseInput * phasePattern[patternPointer + offset]);
+                patternPointer++;
+                if (patternPointer + offset == phasePattern.Length)
+                {
+                    patternPointer = 0;
+                    offset = 0;
+                }
+            }
+            return Math.Abs(output.Sum() % 10);
+        }
+    }
+}
